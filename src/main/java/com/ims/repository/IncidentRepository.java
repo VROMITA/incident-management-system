ed4bc4aadce24b8bc6cae5f6be8ccc3a79dfd712
VROMITA <114232053+VROMITA@users.noreply.github.com>
2026-04-17 21:50:38 +0200

    Add findAll method in InccidentRepository

--- src/main/java/com/ims/repository/IncidentRepository.java
+++ src/main/java/com/ims/repository/IncidentRepository.java
@@ -1,18 +1,27 @@
 package com.ims.repository;
 
 import com.ims.model.Incident;
+import com.ims.model.IncidentSource;
+import com.ims.model.IncidentStatus;
+import com.ims.model.Priority;
 
 import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class IncidentRepository {
 
     private final DatabaseManager dbManager;
 
 
-    private IncidentRepository(){
+    // Call of the DB Manager
+    public IncidentRepository(){
              this.dbManager = DatabaseManager.getInstance();
     }
 
+
+    // Method to save the Incident into DB
     public void save(Incident incident){
        Connection conn = dbManager.getConnection(); // Get a connection with DB
         // INSERT command
@@ -38,6 +47,7 @@ public class IncidentRepository {
            // execute the query
            stmt.executeUpdate();  // Incident created
 
+           // ResultSet is an object which represent the query result.
            ResultSet keys=stmt.getGeneratedKeys();
 
            if(keys.next()){
@@ -51,5 +61,53 @@ public class IncidentRepository {
 
     }
 
+    // Method to retrieve all the incident and output as list
+    public List<Incident> findAll(){
+
+        List<Incident> incidents = new ArrayList<>();
+        Connection conn = dbManager.getConnection();
+        String sql = "SELECT * FROM incidents";
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+
+            // For the attributes not included in the constructor as parameter the setter is used
+            while(rs.next()) {
+
+                Incident incident = new Incident(
+
+                rs.getString("title"),
+                Priority.valueOf(rs.getString("priority")),
+                IncidentSource.valueOf(rs.getString("source"))
+                );
+
+                incident.setId(rs.getInt("id"));
+                incident.setStatus(IncidentStatus.valueOf(rs.getString("status")));
+                incident.setDescription(rs.getString("description"));
+                incident.setAssignedTo(rs.getString("assigned_to"));
+                incident.setStartDate(LocalDateTime.parse(rs.getString("start_date")));
+
+                // End date can be null in the DB therefore a variable is declared to avoid DB crash and is checked before the set method with an If
+                String endDate = rs.getString("end_date");
+                if(endDate != null) {
+                    incident.setEndDate(LocalDateTime.parse(rs.getString("end_date")));
+
+                }
+
+                incidents.add(incident);
+            }
+
+            return incidents; // return the list of the incidents
+
+        } catch (Exception e) {
+            System.out.println("Error on retrieve incidents: " + e.getMessage());
+        }
+
+        return incidents; // Second return in case some error occurs, return an empty list with the above error
+
+
+    }
 
 }
